30c138dbd1f8cf474b9ab2f00d8475cc44b099a1
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2024-07-17 17:54:11 -0400

    addHomePage done

---
+++ Pages/AddHome.cshtml.cs
@@ -0,0 +1,30 @@
+using AspNETcore.BSR.Models;
+using AspNETcore.BSR.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class AddHomeModel : PageModel
+    {
+        private readonly HomeService _homeService;
+
+        public AddHomeModel(HomeService homeService)
+        {
+            _homeService = homeService;
+        }
+
+        [BindProperty]
+        public Home NewHome { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _homeService.AddHome(NewHome);
+            return RedirectToPage("Index");
+        }
+    }
+}
