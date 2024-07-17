383fc437c3d73d410580a1c01a104d79f10deb5b
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2024-07-17 17:40:46 -0400

    DI done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using AspNETcore.BSR.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton<HomeService>();
 
 var app = builder.Build();
 
