4f25b59efffa66c8d89443f6e564bca2d3deaf53
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2024-08-16 14:49:40 -0400

    Filtering completed

--- Models/HomesViewModel.cs
+++ Models/HomesViewModel.cs
@@ -2,4 +2,8 @@ namespace AspNETcore.BSR.Models;
 public class HomesViewModel
 {
     public List<Home>? Homes { get; set; } = [];
+    public int? MinPrice { get; set; }
+    public int? MaxPrice { get; set; }
+    public int? MinArea { get; set; }
+    public int? MaxArea { get; set; }
 }
